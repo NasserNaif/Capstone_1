1cb1e2ac83a7a5e056c727aff19312d7d42abc45
Nasser <nmaif7788@hotmail.com>
2023-08-10 13:47:35 +0300

    first commit

---
+++ src/main/java/com/example/capstoneweek3/CapstoneWeek3Application.java
@@ -0,0 +1,13 @@
+package com.example.capstoneweek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneWeek3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneWeek3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/capstoneweek3/CapstoneWeek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstoneweek3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneWeek3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
